63d0bdd3051f598f238a85aaa9b56c2552f1abb1
Ryan <90624129+rheien@users.noreply.github.com>
2022-05-19 16:00:58 +0200

    #1 create landing page

---
+++ src/Questions/singleChoiceQuestions.ts
@@ -0,0 +1,62 @@
+let singleChoiceQuestions = [
+    
+    {
+        question : "Wie schreibt man",
+        answers : {
+            a : 'Portemonnaie',
+            b : 'Portmonnaie',
+            c : 'Portemonaie'
+        }, 
+        correctAnswer : 'a'
+    },
+
+    {
+        question : "Wie schreibt man",
+        answers : {
+            a : 'Lapallie',
+            b : 'Lapalie',
+            c : 'Lappalie'
+        }, 
+        correctAnswer : 'c'
+    },
+
+    {
+        question : "Wie schreibt man",
+        answers : {
+            a : 'dillettantisch',
+            b : 'dilletantisch',
+            c : 'dilettantisch'
+        }, 
+        correctAnswer : 'c'
+    },
+
+    {
+        question : "Wie schreibt man",
+        answers : {
+            a : 'unentgeltlich',
+            b : 'unentgeldlich',
+            c : 'unendgeltlich'
+        }, 
+        correctAnswer : 'a'
+    },
+
+    {
+        question : "Wie schreibt man",
+        answers : {
+            a : 'Renomee',
+            b : 'Renommee',
+            c : 'Renomme'
+        }, 
+        correctAnswer : 'b'
+    },
+
+    {
+        question : "Wie hieß Ryan früher mal?",
+        answers : {
+            a : 'Le Linh Tung',
+            b : 'Le Chinh Tuan',
+            c : 'Le Duc Anh'
+        }, 
+        correctAnswer : 'c'
+    },
+];
