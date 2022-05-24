882e8a3b0cc6b626ea4f7fe905424dff0075a424
Ryan Le <ryan.le@axelspringer.com>
2022-05-24 10:51:46 +0200

    #2 rendering question page

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import {multipleChoiceQuestions} from "./Questions/multipleChoiceQuestions.js";
- import {singleChoiceQuestions} from "./Questions/singleChoiceQuestions.js";
- import {textInputQuestions} from "./Questions/textInputQuestions.js";
+import { multipleChoiceQuestions } from "./Questions/multipleChoiceQuestions.js";
+import { singleChoiceQuestions } from "./Questions/singleChoiceQuestions.js";
+import { textInputQuestions } from "./Questions/textInputQuestions.js";
 
+let b = multipleChoiceQuestions;
+let c = singleChoiceQuestions;
+let d = textInputQuestions;
 
- let b = multipleChoiceQuestions;
- let c = singleChoiceQuestions;
- let d= textInputQuestions;
+const button = document.querySelector("#start")
+button.addEventListener("click", buildQuizPage);
 
- const button = document.querySelector("#start")
- button.addEventListener("click",buildQuizPage);
+function buildQuizPage() {
+    let body = document.body;
+    body.removeChild(body.children[1])
 
- function buildQuizPage() {
-     let body = document.body;
-     body.removeChild(body.children[1])
-     body.removeChild(body.children[1])
-     body.removeChild(body.children[1])
+    /* create score bar */
+    let scoreBar = document.createElement('div');
+    scoreBar.className = 'center bar';
+    scoreBar.textContent = 'Number of answered questions:';
 
-     let scoreBar = document.createElement('div');
-     scoreBar.className = 'center bar';
-     scoreBar.textContent = 'Number of answered questions:';
-
-
-     let circleList = document.createElement('ul');
-     for(let i = 1; i < 7 ; i++){
+    let circleList = document.createElement('ul');
+    for (let i = 1; i < 7; i++) {
         let circle = document.createElement('li');
-        circleList.id = 'Question'+i;
+        circleList.id = 'Question' + i;
         circleList.appendChild(circle);
         scoreBar.appendChild(circleList);
-        body.appendChild(scoreBar);
-     }
+    }
+    let header = document.getElementsByClassName('header')[0];
+    header.appendChild(scoreBar);
 
-     let questionContainer = document.createElement('div');
-     questionContainer.className = 'center container';
-     
+    body.appendChild(document.createElement('br'));
 
-     let submitButton = document.createElement('button');
-     submitButton.className = 'btn submit';
-     submitButton.setAttribute('type','button');
-     submitButton.textContent = 'SUBMIT';
-     questionContainer.appendChild(submitButton);
-     body.appendChild(questionContainer);
- };
+    /* create container for question, answer options & submit button  */
+    let questionContainer = document.createElement('div');
+    questionContainer.className = 'center container';
+
+    let submitButton = document.createElement('button');
+    submitButton.className = 'btn submit';
+    submitButton.setAttribute('type', 'button');
+    submitButton.textContent = 'SUBMIT';
+    questionContainer.appendChild(submitButton);
+    body.appendChild(questionContainer);
+};
