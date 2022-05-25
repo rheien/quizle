d6338026304617cfee808b0d9346f13970c38f18
Ryan Le <ryan.le@axelspringer.com>
2022-05-25 17:28:26 +0200

    #3 create question cards for building a quiz

--- src/ts/questionCard.ts
+++ src/ts/questionCard.ts
@@ -2,23 +2,46 @@ import { multipleChoiceQuestions } from "../questions/multipleChoiceQuestions.js
 import { singleChoiceQuestions } from "../questions/singleChoiceQuestions.js";
 import { textInputQuestions } from "../questions/textInputQuestions.js";
 
-/* multipleChoiceQuestions = {
-    question, answers, correctAnswers
-} */
 let b : object = multipleChoiceQuestions;
 
-/* singleChoiceQuestions = {
-    question, answers, correctAnswer
-} */
 let c : object = singleChoiceQuestions;
 
-/* textInputQuestions = {
-    question, answer, correctAnswer
-} */
 let d : object= textInputQuestions;
 
- export function questionCard(){
+interface Question {
+    question: string;
+};
 
+interface MultipleChoice extends Question{
+    answers: string;
+    correctAnswers: string;
+};
+
+interface SingleChoice extends Question{
+    answers: string;
+    correctAnswer: string;
+};
+
+interface TextInput extends Question{
+    answers: string;
+    correctAnswer: string;
+};
+
+
+ export function questionCard(): {question : string, answers : string, correctAnswers : string} []{
+    const quiz : {question : string, answers : string, correctAnswers : string} []= [];
+
+    for (let index = 0; index < Object.entries(b).length; index++) {
+        let question = b[index].question;
+        let answers = b[index].answers;
+        let correctAnswers = b[index].correctAnswers;
+        quiz.push({question,answers,correctAnswers});
+    }
+    console.dir(quiz);
+
+    return quiz;
 
     /** later a shuffle function? */
-}
+};
+
+questionCard();
