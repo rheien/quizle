ba44540851312d222381295ef2d75a0a52f03b03
Ryan Le <ryan.le@axelspringer.com>
2022-05-27 15:16:16 +0200

    #3 script pick first two question from each type for question page

--- src/ts/questionCard.ts
+++ src/ts/questionCard.ts
@@ -1,12 +1,13 @@
 import { multipleChoiceQuestions } from "../questions/multipleChoiceQuestions.js";
 import { singleChoiceQuestions } from "../questions/singleChoiceQuestions.js";
 import { textInputQuestions } from "../questions/textInputQuestions.js";
+let assert = require('assert');
 
-let b : object = multipleChoiceQuestions;
+let mChoice : object = multipleChoiceQuestions;
 
-let c : object = singleChoiceQuestions;
+let sChoice : object = singleChoiceQuestions;
 
-let d : object= textInputQuestions;
+let tInput : object= textInputQuestions;
 
 interface Question {
     question: string;
@@ -19,29 +20,67 @@ interface MultipleChoice extends Question{
 
 interface SingleChoice extends Question{
     answers: string;
-    correctAnswer: string;
+    correctAnswers: string;
 };
 
 interface TextInput extends Question{
     answers: string;
-    correctAnswer: string;
+    correctAnswers: string;
 };
 
 
  export function questionCard(): {question : string, answers : string, correctAnswers : string} []{
     const quiz : {question : string, answers : string, correctAnswers : string} []= [];
 
-    for (let index = 0; index < Object.entries(b).length; index++) {
-        let question = b[index].question;
-        let answers = b[index].answers;
-        let correctAnswers = b[index].correctAnswers;
-        quiz.push({question,answers,correctAnswers});
+    //for (let index = 0; index < Object.entries(b).length; index++) {
+    
+    /* later change index  for random question pick */
+    for (let index = 0; index < 2; index++) {    
+        let card : MultipleChoice = {
+            question : mChoice[index].question,
+            answers : mChoice[index].answers,
+            correctAnswers : mChoice[index].correctAnswers
+        }
+        quiz.push(card);
     }
+
+    for (let index = 0; index < 2; index++) {    
+        let card : SingleChoice = {
+            question : sChoice[index].question,
+            answers : sChoice[index].answers,
+            correctAnswers : sChoice[index].correctAnswer
+        }
+        quiz.push(card);
+    }
+
+    for (let index = 0; index < 2; index++) {    
+        let card : TextInput = {
+            question : tInput[index].question,
+            answers : tInput[index].answer,
+            correctAnswers : tInput[index].correctAnswer
+        }
+        quiz.push(card);
+    }
+
     console.dir(quiz);
-
+    console.dir(quiz[5].question);
+    checkRepetition(quiz);
     return quiz;
+};
+ 
 
-    /** later a shuffle function? */
+/** later a shuffle function? */
+
+
+/*______________________________________________________*/
+function checkRepetition (list : {}[] ) : boolean{
+    list.forEach((element,index) => {
+        if (element[index].question)  {
+            assert.deepEqual()
+        }  
+    });
+    
+    return true
 };
 
 questionCard();
