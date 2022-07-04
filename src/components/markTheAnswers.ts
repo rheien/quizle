ffa2f1d242ed60003748410fa7a17952ec5675be
Ryan Le <ryan.le@axelspringer.com>
2022-07-04 13:38:04 +0200

    #4 move to a seperate file

---
+++ src/components/markTheAnswers.ts
@@ -0,0 +1,28 @@
+import { Quiz } from "./Quiz";
+
+/** set id for selected answers */
+export function markTheAnswers(quiz: Quiz, collectedAnswers: string[]) {
+    let question = quiz.questions[quiz.round];
+    collectedAnswers.forEach(answer => {
+        let indexAnswer = question.answers.indexOf(answer);
+        let coloredAnswer = document.getElementById("answer_" + indexAnswer.toString());
+
+        if (quiz.answeredCorrectly(question.correctAnswers, answer)) {
+            coloredAnswer?.setAttribute("id", "right");
+            
+            // in case of lower case answer in free text input
+            if (indexAnswer === -1) {
+                coloredAnswer = document.getElementById("answer_0");
+                coloredAnswer?.setAttribute("id", "right");
+            }
+        }
+        else {
+            coloredAnswer?.setAttribute("id", "wrong");
+            
+            if (indexAnswer === -1) {
+                coloredAnswer = document.getElementById("answer_0");
+                coloredAnswer?.setAttribute("id", "wrong");
+            }
+        }
+    });
+};
