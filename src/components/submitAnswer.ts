d97ee7af5ce42afad97325dd5961e932c7f7722e
Ryan Le <ryan.le@axelspringer.com>
2022-07-05 15:30:30 +0200

    #5 move the function markTheAnswers back to submitAnswers

--- src/components/submitAnswer.ts
+++ src/components/submitAnswer.ts
@@ -1,13 +1,19 @@
 import { collectSelectedAnswers } from "./collectSelectedAnswers";
 import { hideButton } from "./hideButton";
+import { markTheAnswers } from "./markTheAnswers";
 import { Quiz } from "./Quiz";
 import { QuizMaster } from "./QuizMaster";
 
 export function submitAnswer(quizMaster: QuizMaster, quiz: Quiz) {
-    hideButton('submit');
-    hideButton('next');
-
+    
     let questions = quiz.questions[quiz.round];
     let collectedAnswers = collectSelectedAnswers(questions);
-    quizMaster.handleQuizScore(quiz, collectedAnswers);
-}
+    if(collectedAnswers.length !== 0){
+        hideButton('submit');
+        hideButton('next');
+        markTheAnswers(quiz,collectedAnswers);
+        quizMaster.handleQuizScore(quiz, collectedAnswers);
+    }
+   
+
+};
