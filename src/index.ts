22184c55404732b6e7c4259cc77a3051db991240
Ryan Le <ryan.le@axelspringer.com>
2022-08-02 11:39:15 +0200

    #27 add environment variable for the port

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
 app.get('/', (req, res ) => {
     res.send('index');
 })
-
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
