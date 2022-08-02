ed64f8d0e3e97e9bed7a4575b51517e48ccec66a
Ryan Le <ryan.le@axelspringer.com>
2022-08-02 14:20:34 +0200

    testing other environment variables

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,24 @@ const app = express();
 
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT || 3000;
+
+let port;
+process.env.STATUS === 'production'
+    ? (port = process.env.PROD_PORT)
+    : (port = process.env.DEV_PORT);
 
 app.use(express.static('dist'));
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    //res.status(404).send('Something broke!');
+    next(err);
+});
 
 app.get('/', (req, res ) => {
     res.send('index');
 })
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
+    console.log(`${process.env.STATUS} Server running at http://localhost:${port}`)
 });
