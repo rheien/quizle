44bd4661cddfb85c30e7cb55be1daa72ce5931c6
Ryan Le <ryan.le@axelspringer.com>
2022-08-11 10:55:00 +0200

    refactor

--- src/presentation/Answer.ts
+++ src/presentation/Answer.ts
@@ -2,62 +2,62 @@ import { QuizMaster } from "../game/QuizMaster";
 import { Question, QuestionType } from "../questions/types";
 import { AddButton } from "./AddButton";
 
-export class Answer{
+export class Answer {
 
     /* This method collect the answers for QuizMaster */
     collectSelectedAnswers(question: Question): string[] {
         let collectedAnswers: string[] = [];
-    
-        if(question.type === QuestionType.FREE_TEXT){
+
+        if (question.type === QuestionType.FREE_TEXT) {
             const input = document.getElementById('0') as HTMLInputElement;
-            if(this.hasAnswered(input)){
+            if (this.hasAnswered(input)) {
                 collectedAnswers.push(input.value);
             }
             return collectedAnswers;
         }
-    
+
         else if (question.type === QuestionType.MULTIPLE_CHOICE) {
             for (let index = 0; index < question.answers.length; index++) {
                 const answerType = document.getElementById(index.toString()) as HTMLInputElement;
-    
+
                 if (answerType.checked === true) {
                     collectedAnswers.push(answerType.value);
                 }
             }
             return collectedAnswers;
         }
-    
+
         else if (question.type === QuestionType.SINGLE_CHOICE) {
             for (let index = 0; index < question.answers.length; index++) {
                 const answerType = document.getElementById(index.toString()) as HTMLInputElement;
-    
+
                 if (answerType.checked === true) {
                     collectedAnswers.push(answerType.value);
                 }
             }
             return collectedAnswers;
         }
-    
+
         throw new Error(`Unsupported questionType: ${question.type}`)
     };
-    
-    hasAnswered(input: HTMLInputElement): boolean{
+
+    hasAnswered(input: HTMLInputElement): boolean {
         return input.value.length !== 0
     };
 
-    coloursScoreBar(quizMaster: QuizMaster, collectedAnswers: string[]){
+    coloursScoreBar(quizMaster: QuizMaster, collectedAnswers: string[]) {
         let round = quizMaster.quiz.round;
         let question: Question = quizMaster.quiz.questions[round];
 
-        let coloredScoreBar = document.getElementById("Question"+(round+1).toString()) as HTMLSpanElement;
-        
-        if(quizMaster.evaluateAnswers(question,collectedAnswers)){
+        let coloredScoreBar = document.getElementById("Question" + (round + 1).toString()) as HTMLSpanElement;
+
+        if (quizMaster.evaluateAnswers(question, collectedAnswers)) {
             coloredScoreBar.setAttribute("id", "circle_correct");
         }
-        else{
+        else {
             coloredScoreBar.setAttribute("id", "circle_wrong");
         }
-            
+
     };
 
     marksTheAnswers(quizMaster: QuizMaster, collectedAnswers: string[]) {
@@ -65,52 +65,51 @@ export class Answer{
         collectedAnswers.forEach(answer => {
             let indexAnswer = question.answers.indexOf(answer);
             let coloredAnswer = document.getElementById("answer_" + indexAnswer.toString()) as HTMLDivElement;
-    
+
             if (quizMaster.quiz.answeredCorrectly(question.correctAnswers, answer)) {
-                
+
                 // in case of lower case answer in free text input
                 if (indexAnswer === -1) {
                     coloredAnswer = document.getElementById("answer_0") as HTMLDivElement;
                     coloredAnswer.setAttribute("id", "correct");
                 }
-                else{
+                else {
                     coloredAnswer.setAttribute("id", "correct");
                 }
-                
+
             }
             else {
                 if (indexAnswer === -1) {
                     coloredAnswer = document.getElementById("answer_0") as HTMLDivElement;
                     coloredAnswer.setAttribute("id", "wrong");
                 }
-                else{
+                else {
                     coloredAnswer.setAttribute("id", "wrong");
                 }
             }
         });
     };
 
-    noticeBox(): void{
+    noticeBox(): void {
         alert("Some answers are missing!!!");
+        //TODO: replace alert with a better solution?
         //const note = document.getElementById("");
-    }
+    };
 
     submitsAnswer(quizMaster: QuizMaster) {
-    
+
         let questions = quizMaster.quiz.questions[quizMaster.quiz.round];
         let collectedAnswers = this.collectSelectedAnswers(questions);
-        
-
 
         const addButton = new AddButton();
-        if(collectedAnswers.length !== 0){
+        if (collectedAnswers.length !== 0) {
             addButton.hideButton('submit');
             addButton.hideButton('next');
 
-            this.marksTheAnswers(quizMaster,collectedAnswers);
-            this.coloursScoreBar(quizMaster,collectedAnswers);
-            if(questions.type === QuestionType.MULTIPLE_CHOICE){
-                if(!quizMaster.evaluateAnswers(quizMaster.quiz.questions[quizMaster.quiz.round],collectedAnswers)){
+            this.marksTheAnswers(quizMaster, collectedAnswers);
+            this.coloursScoreBar(quizMaster, collectedAnswers);
+            if (questions.type === QuestionType.MULTIPLE_CHOICE) {
+                if (!quizMaster.evaluateAnswers(questions, collectedAnswers)) {
                     this.noticeBox();
                 }
             }
